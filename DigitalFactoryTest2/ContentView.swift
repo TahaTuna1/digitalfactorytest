bd6b0f639ba0ae430515d8915d93d3c5e988bdba
Taha Tuna <119931873+TahaTuna1@users.noreply.github.com>
2024-03-09 23:04:08 +0300

    Initial Commit

---
+++ DigitalFactoryTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigitalFactoryTest2
+//
+//  Created by Taha Tuna on 9.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigitalFactoryTest2/DigitalFactoryTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalFactoryTest2App.swift
+//  DigitalFactoryTest2
+//
+//  Created by Taha Tuna on 9.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DigitalFactoryTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
